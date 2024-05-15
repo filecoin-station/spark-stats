e93d7e159ac9ed88436158847c15c3e3b5ce188e
Julian Gruber <julian@juliangruber.com>
2024-05-15 13:44:35 +0200

    run `@sentry/migr`, fix style

--- bin/spark-stats.js
+++ bin/spark-stats.js
@@ -1,7 +1,7 @@
+import '../lib/instrument.js'
 import http from 'node:http'
 import { once } from 'node:events'
 import pg from 'pg'
-import Sentry from '@sentry/node'
 import fs from 'node:fs/promises'
 import { join, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -26,13 +26,6 @@ const pkg = JSON.parse(
     'utf8'
   )
 )
-
-Sentry.init({
-  dsn: 'https://47b65848a6171ecd8bf9f5395a782b3f@o1408530.ingest.sentry.io/4506576125427712',
-  release: pkg.version,
-  environment: SENTRY_ENVIRONMENT,
-  tracesSampleRate: 0.1
-})
 
 const pgPool = new pg.Pool({
   connectionString: DATABASE_URL,
