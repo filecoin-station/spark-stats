a4c7f5107a2834892ba61c5f7cd44a4d9ce95ca1
Julian Gruber <julian@juliangruber.com>
2024-06-07 06:01:34 -0700

    Update observer/lib/platform-stats.js

--- observer/lib/platform-stats.js
+++ observer/lib/platform-stats.js
@@ -10,7 +10,6 @@ export const updateDailyTransferStats = async (pgClient, transferEvent, currentB
     INSERT INTO daily_reward_transfers (day, to_address, amount, last_checked_block)
     VALUES (now(), $1, $2, $3)
     ON CONFLICT (day, to_address) DO UPDATE SET
-      amount = daily_reward_transfers.amount + EXCLUDED.amount,
-      last_checked_block = EXCLUDED.last_checked_block
+      amount = daily_reward_transfers.amount + EXCLUDED.amount
   `, [transferEvent.to_address, transferEvent.amount, currentBlockNumber])
 }
