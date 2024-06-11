5b8a082937d8597716b096b4655a4ba9bd5746c6
Julian Gruber <julian@juliangruber.com>
2024-06-11 23:53:06 +0200

    fix migrate bin

--- observer/bin/migrate.js
+++ observer/bin/migrate.js
@@ -1,5 +1,13 @@
-import { getStats } from '../../common/db.js'
+import { getPgPools } from '../../common/db.js'
+import { migrateWithPgClient as migrateEvaluateDB } from 'spark-evaluate/lib/migrate.js'
 import { migrateWithPgClient as migrateStatsDB } from '@filecoin-station/spark-stats-db-migrations'
 
+const pgPools = await getPgPools()
+
+console.log('Migrating spark_evaluate database')
+await migrateEvaluateDB(pgPools.evaluate)
+
 console.log('Migrating spark_stats database')
-await migrateStatsDB(await getStats())
+await migrateStatsDB(pgPools.stats)
+
+await pgPools.end()
