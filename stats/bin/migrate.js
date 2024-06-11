d4d3ce18b51ca39ca8317e2cb49f9da4047695ea
Miroslav Bajtoš <oss@bajtos.net>
2024-06-11 16:16:46 +0200

    feat: expose "total" and "successful" for RSR (#135)
    Improve the following two endpoints to return the number of all checks
    and the number of successful checks that were used to calculate RSR.
    This enables consumers to filter out some data points and still be able
    to correctly calculate aggregated RSR.
    
    - `/miners/retrieval-success-rate/summary`
    - `/retrieval-success-rate`
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- stats/bin/migrate.js
+++ stats/bin/migrate.js
@@ -5,8 +5,8 @@ import {
 import { migrateWithPgConfig as migrateEvaluateDB } from 'spark-evaluate/lib/migrate.js'
 import { migrateWithPgConfig as migrateStatsDB } from '@filecoin-station/spark-stats-db-migrations'
 
-console.log('Migrating spark_evaluate database')
+console.log('Migrating spark_evaluate database', EVALUATE_DB_URL)
 await migrateEvaluateDB({ connectionString: EVALUATE_DB_URL })
 
-console.log('Migrating spark_stats database')
+console.log('Migrating spark_stats database', DATABASE_URL)
 await migrateStatsDB({ connectionString: DATABASE_URL })
