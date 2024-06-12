9e18617538416e12813615f6a2ecf930da380d99
Julian Gruber <julian@juliangruber.com>
2024-06-12 13:42:01 +0200

    remove migrate evaluate db

--- stats/bin/migrate.js
+++ stats/bin/migrate.js
@@ -1,12 +1,8 @@
 import {
-  DATABASE_URL,
-  EVALUATE_DB_URL
+  DATABASE_URL
 } from '../lib/config.js'
-import { migrateWithPgConfig as migrateEvaluateDB } from 'spark-evaluate/lib/migrate.js'
 import { migrateWithPgConfig as migrateStatsDB } from '@filecoin-station/spark-stats-db-migrations'
 
-console.log('Migrating spark_evaluate database', EVALUATE_DB_URL)
-await migrateEvaluateDB({ connectionString: EVALUATE_DB_URL })
 
 console.log('Migrating spark_stats database', DATABASE_URL)
 await migrateStatsDB({ connectionString: DATABASE_URL })
