747e79cd26fbecd6c7975f10673472083d0e5438
Julian Gruber <julian@juliangruber.com>
2024-06-12 13:43:05 +0200

    fix lint

--- stats/bin/migrate.js
+++ stats/bin/migrate.js
@@ -3,6 +3,5 @@ import {
 } from '../lib/config.js'
 import { migrateWithPgConfig as migrateStatsDB } from '@filecoin-station/spark-stats-db-migrations'
 
-
 console.log('Migrating spark_stats database', DATABASE_URL)
 await migrateStatsDB({ connectionString: DATABASE_URL })
