e0061b673ca85c0975fd6ee4c28f1399e724030c
Julian Gruber <julian@juliangruber.com>
2024-06-12 17:06:05 +0200

    add day to result

--- stats/lib/stats-fetchers.js
+++ stats/lib/stats-fetchers.js
@@ -120,7 +120,7 @@ export const fetchParticipantChangeRates = async (pgPools, filter) => {
  */
 export const fetchParticipantScheduledRewards = async (pgPools, filter) => {
   const { rows } = await pgPools.stats.query(`
-    SELECT scheduled_rewards
+    SELECT day::text, scheduled_rewards
     FROM daily_scheduled_rewards
     WHERE participant_address = $1 AND day >= $2 AND day <= $3
    `, [
