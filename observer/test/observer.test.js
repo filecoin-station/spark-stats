34981fdfe258f239f1db5d903197920e053f0acf
Miroslav Bajtoš <oss@bajtos.net>
2024-09-02 16:34:28 +0200

    revert! spark-evaluate related change
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- observer/test/observer.test.js
+++ observer/test/observer.test.js
@@ -128,8 +128,6 @@ describe('observer', () => {
 
   describe('observeScheduledRewards', () => {
     beforeEach(async () => {
-      await pgPools.evaluate.query('DELETE FROM recent_station_details')
-      await pgPools.evaluate.query('DELETE FROM recent_participant_subnets')
       await pgPools.evaluate.query('DELETE FROM daily_participants')
       await pgPools.evaluate.query('DELETE FROM participants')
       await pgPools.stats.query('DELETE FROM daily_scheduled_rewards')
