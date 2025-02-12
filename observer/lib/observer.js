filecoin-station/spark-stats

commit cbb3bf177319c8d938cc638f4d0b1ec11faa09ae

perf: optimise SQL query in observeRetrievalResultCodes (#316)
Rework `observeRetrievalResultCodes` to execute a single SQL query
to update all daily codes in one go.

Before this change, we would run ~1k individual queries, consume a lot
of CPU and trigger CPU throttling alert.

As part of this change, I am also adding a console log to tell us how
many rows - tuples (day, code) - we are updating in each loop iteration.

Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

observer/lib/observer.js
@@ -106,6 +106,7 @@ export const observeRetrievalResultCodes = async (pgPoolStats, influxQueryApi) =
   // Ref: https://github.com/filecoin-station/spark-stats/pull/244#discussion_r1824808007
   // Note: Having a bucket retention policy is important for this query not to
   // time out.
+  /** @type {{_time: string; _field: string; _value: number}[]} */
   const rows = await influxQueryApi.collectRows(`
     import "strings"
     from(bucket: "spark-evaluate")
@@ -116,16 +117,16 @@ export const observeRetrievalResultCodes = async (pgPoolStats, influxQueryApi) =
       |> keep(columns: ["_value", "_time", "_field"])
       |> map(fn: (r) => ({ r with _field: strings.replace(v: r._field, t: "result_rate_", u: "", i: 1) }))
   `)
-  for (const row of rows) {
-    await pgPoolStats.query(`
-      INSERT INTO daily_retrieval_result_codes
-      (day, code, rate)
-      VALUES ($1, $2, $3)
-      ON CONFLICT (day, code) DO UPDATE SET rate = EXCLUDED.rate
-    `, [
-      row._time,
-      row._field,
-      row._value
-    ])
-  }
+  console.log('Inserting %s rows to daily_retrieval_result_codes ', rows.length)
+
+  await pgPoolStats.query(`
+    INSERT INTO daily_retrieval_result_codes
+    (day, code, rate)
+    VALUES (unnest($1::DATE[]), unnest($2::TEXT[]), unnest($3::NUMERIC[]))
+    ON CONFLICT (day, code) DO UPDATE SET rate = EXCLUDED.rate
+  `, [
+    rows.map(r => r._time),
+    rows.map(r => r._field),
+    rows.map(r => r._value)
+  ])
 }

commit e724cd9dd2875d459662f8589f5ee971ed74409c

fix: day returned by InfluxDB query
The function `observeRetrievalResultCodes()` includes the following
statement in the InfluxDB query fetching `(day, code, rate)` data:

    |> aggregateWindow(every: 1d, fn: mean, createEmpty: false)

Such query produces a list of values like this:

```
2024-11-15T00:00:00Z,CONNECTION_REFUSED,0.0022313570194142725
2024-11-16T00:00:00Z,CONNECTION_REFUSED,0.002153071995819862
(...)
2025-02-12T00:00:00Z,CONNECTION_REFUSED,0.021266890041248942
2025-02-12T13:08:20.817239423Z,CONNECTION_REFUSED,0.02153170594662248
```

Notice there are two rows for today (2025-02-12). One row contains
data from yesterday (full day) and another row contain partial data from
today.

In this commit, I am fixing the query to correctly assign data points
from yesterday to yesterday's date:

      |> aggregateWindow(every: 1d, fn: mean, createEmpty: false, timeSrc: "_start")

The new query produces a list like this:

```
2024-11-14T00:00:00Z,CONNECTION_REFUSED,0.0022313570194142725
2024-11-15T00:00:00Z,CONNECTION_REFUSED,0.002153071995819862
(...)
2025-02-11T00:00:00Z,CONNECTION_REFUSED,0.021266890041248942
2025-02-12T00:00:00Z,CONNECTION_REFUSED,0.02153170594662248
```

This fixed the error introduced by cbb3bf17 (#316), where the SQL query
fails with the following message:

```
ON CONFLICT DO UPDATE command cannot affect row a second time.
Ensure that no rows proposed for insertion within the same command have duplicate constrained values.
```

See also InfluxDB documentation for `aggregateWindow()`:
https://docs.influxdata.com/flux/v0/stdlib/universe/aggregatewindow/#timesrc

Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

observer/lib/observer.js
@@ -113,7 +113,7 @@ export const observeRetrievalResultCodes = async (pgPoolStats, influxQueryApi) =
       |> range(start: 0)
       |> filter(fn: (r) => r["_measurement"] == "retrieval_stats_honest")
       |> filter(fn: (r) => strings.hasPrefix(v: r._field, prefix: "result_rate_"))
-      |> aggregateWindow(every: 1d, fn: mean, createEmpty: false)
+      |> aggregateWindow(every: 1d, fn: mean, createEmpty: false, timeSrc: "_start")
       |> keep(columns: ["_value", "_time", "_field"])
       |> map(fn: (r) => ({ r with _field: strings.replace(v: r._field, t: "result_rate_", u: "", i: 1) }))
   `)